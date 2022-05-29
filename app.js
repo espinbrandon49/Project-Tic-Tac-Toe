38bc857ff197b657f4094f050cd49f4bc2d12d7d
espinbrandon49 <102924713+espinbrandon49@users.noreply.github.com>
2022-05-29 12:37:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -25,35 +25,6 @@ const game = (() => {
     const turn = (() => {
       let count = 0
       const square1 = document.getElementById(1)
-
-      const square2 = document.getElementById(2)
-
-      for (let i = 1; i <= 9; i++) {
-        let gameSpace = document.getElementById(i)
-        const playerMove = () => {
-          if (gameSpace.textContent == '') {
-            if (count % 2 == 0) {
-              gameSpace.textContent = player1.marker
-              gameSpace.setAttribute('value', 1)   
-              console.log(gameSpace.getAttribute('value'))    
-            } else {
-              gameSpace.textContent = player2.marker
-              gameSpace.setAttribute('value', 2)   
-              console.log(gameSpace.getAttribute('value'))  
-            }
-            count++
-            console.log(parseInt(square1.getAttribute('value')) + parseInt(square2.getAttribute('value'))
-            )
-
-          }
-        }
-    
-        gameSpace.addEventListener('click', playerMove)
-      }
-    })()
-
-    const gameOver = () => {
-      const square1 = document.getElementById(1)
       const square2 = document.getElementById(2)
       const square3 = document.getElementById(3)
       const square4 = document.getElementById(4)
@@ -62,9 +33,42 @@ const game = (() => {
       const square7 = document.getElementById(7)
       const square8 = document.getElementById(8)
       const square9 = document.getElementById(9)
-      
-    }
-  //  gameOver()
+
+      for (let i = 1; i <= 9; i++) {
+        let gameSpace = document.getElementById(i)
+        const playerMove = () => {
+          if (gameSpace.textContent == '') {
+            if (count % 2 == 0) {
+              gameSpace.textContent = player1.marker
+              gameSpace.setAttribute('value', 1)  
+            } else {
+              gameSpace.textContent = player2.marker
+              gameSpace.setAttribute('value', 2)
+            }
+
+            count++
+            const lineScore = (squareA, squareB, squareC) => {
+              squareA = parseInt(squareA.getAttribute('value'))
+              squareB = parseInt(squareB.getAttribute('value'))
+              squareC = parseInt(squareC.getAttribute('value'))
+              const score = () => squareA + squareB + squareC
+              return {score, squareA, squareB, squareC}
+            }
+
+            const lineScore1 = lineScore(square1, square2, square3)
+            console.log(lineScore1.score())
+            const lineScore2 = lineScore(square1, square4, square7)
+            console.log(lineScore2.score())
+            console.log(lineScore1)
+            console.log(lineScore2)
+            
+          }
+        }
+        gameSpace.addEventListener('click', playerMove)
+      }
+    })()
+
+
 
   })()
 
@@ -77,43 +81,54 @@ const game = (() => {
 122 = 5
 222 = 6*/
 
-      /*
-      const scores = (squareA, squareB, squareC) => {
-        squareA = squareA
-        squareB = squareB
-        squareC = squareC
-        let score = 0
-        squareA.addEventListener('click', () => {
-          if (squareA.getAttribute('data-value') != 0) {
-            score += parseInt(squareA.getAttribute('value'))
-          }
-          console.log(score)
-          squareA.setAttribute('data-value', 0)
-        })
-        squareB.addEventListener('click', () => {
-          if (squareB.getAttribute('data-value') != 0) {
-            score += parseInt(squareB.getAttribute('value'))
-          }
-          console.log(score)
-          squareB.setAttribute('data-value', 0)
-        })
-        squareC.addEventListener('click', () => {
-          if (squareC.getAttribute('data-value') != 0) {
-            score += parseInt(squareC.getAttribute('value'))
-          }
-          console.log(score)
-          squareC.setAttribute('data-value', 0)
-        })
-     
-        return { squareA, squareB, squareC }
-      }
+/*
+const scores = (squareA, squareB, squareC) => {
+  squareA = squareA
+  squareB = squareB
+  squareC = squareC
+  let score = 0
+  squareA.addEventListener('click', () => {
+    if (squareA.getAttribute('data-value') != 0) {
+      score += parseInt(squareA.getAttribute('value'))
+    }
+    console.log(score)
+    squareA.setAttribute('data-value', 0)
+  })
+  squareB.addEventListener('click', () => {
+    if (squareB.getAttribute('data-value') != 0) {
+      score += parseInt(squareB.getAttribute('value'))
+    }
+    console.log(score)
+    squareB.setAttribute('data-value', 0)
+  })
+  squareC.addEventListener('click', () => {
+    if (squareC.getAttribute('data-value') != 0) {
+      score += parseInt(squareC.getAttribute('value'))
+    }
+    console.log(score)
+    squareC.setAttribute('data-value', 0)
+  })
+ 
+  return { squareA, squareB, squareC }
+}
 
-      const score1 = scores(square1, square2, square3)
-      const score2 = scores(square4, square5, square6)
-      const score3 = scores(square7, square8, square9)
-      const score4 = scores(square1, square5, square9)
-      const score5 = scores(square3, square5, square9)
-      const score6 = scores(square1, square4, square7)
-      const score7 = scores(square2, square5, square8)
-      const score8 = scores(square3, square6, square9)
+const score1 = scores(square1, square2, square3)
+const score2 = scores(square4, square5, square6)
+const score3 = scores(square7, square8, square9)
+const score4 = scores(square1, square5, square9)
+const score5 = scores(square3, square5, square9)
+const score6 = scores(square1, square4, square7)
+const score7 = scores(square2, square5, square8)
+const score8 = scores(square3, square6, square9)
 */
+
+/*            const square1V = parseInt(square1.getAttribute('value'))
+            const square2V = parseInt(square2.getAttribute('value'))
+            const square3V = parseInt(square3.getAttribute('value'))
+            const square4V = parseInt(square4.getAttribute('value'))
+            const square5V = parseInt(square5.getAttribute('value'))
+            const square6V = parseInt(square6.getAttribute('value'))
+            const square7V = parseInt(square7.getAttribute('value'))
+            const square8V = parseInt(square8.getAttribute('value'))
+            const square9V = parseInt(square9.getAttribute('value'))
+            */
