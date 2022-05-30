fd46a9dd54ec379b22c1dcff574ee4e4f90613f8
espinbrandon49 <102924713+espinbrandon49@users.noreply.github.com>
2022-05-29 20:21:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,74 @@
 const game = (() => {
 
-  
   const createGameboard = (() => {
     const body = document.querySelector('body')
 
     const header = (() => {
       const title = document.createElement('div')
+      title.setAttribute('id', 'title')
       const image = document.createElement('img')
       const headerText = document.createElement('h1')
-      const X = document.createElement('button')
-      const Y = document.createElement('button')
-      
       image.setAttribute('src', './odin-lined.png')
-      X.textContent = "X's"
-      Y.textContent = "O's"
       headerText.textContent = 'Tic Tac Toe'
       title.appendChild(image)
       title.appendChild(headerText)
-      title.appendChild(X)
-      title.appendChild(Y)
       body.appendChild(title)
-    })()
 
+      const column3 = (() => {
+        const selectMarker = (() => {
+          const markers = document.createElement('fieldset')
+          markers.setAttribute('id', 'markers')
+          const legend = document.createElement('legend')
+          legend.textContent = 'Select marker (X/starts)'
+
+          const radioX = document.createElement('div')
+          radioX.setAttribute('id', 'radioX')
+          const labelX = document.createElement('label')
+          labelX.setAttribute('for', 'markerX')
+          labelX.textContent = 'X'
+          const X = document.createElement('input')
+          X.setAttribute('type', 'radio')
+          X.setAttribute('id', 'markerX')
+          X.setAttribute('name', 'marker')
+
+          const radioO = document.createElement('div')
+          radioO.setAttribute('id', 'radioO')
+          const labelO = document.createElement('label')
+          labelO.setAttribute('for', 'markerO')
+          labelO.textContent = 'O'
+          const O = document.createElement('input')
+          O.setAttribute('id', 'markerO')
+          O.setAttribute('type', 'radio')
+          O.setAttribute('name', 'marker')
+
+          const blankDiv = document.createElement('div')
+          blankDiv.setAttribute('id', 'blankDiv')
+          const XOR = document.createElement('div')
+          XOR.setAttribute('id', 'XOR')
+
+          markers.appendChild(legend)
+          markers.appendChild(blankDiv)
+          markers.appendChild(XOR)
+          XOR.appendChild(radioX)
+          XOR.appendChild(radioO)
+          radioX.appendChild(X)
+          radioX.appendChild(labelX)
+          radioO.appendChild(O)
+          radioO.appendChild(labelO)
+          title.appendChild(markers)
+
+          const createRestart = (() => {
+            const restart = document.createElement('button')
+            restart.addEventListener('click', () => location.reload())
+            restart.textContent = 'restart'
+            XOR.appendChild(restart)
+          })()
+
+        })()
+
+      })()
+
+    })()
     const createContainer = (() => {
       const container = document.createElement('div')
       container.setAttribute('id', 'container')
@@ -35,15 +82,6 @@ const game = (() => {
         }
       })()
     })()
-    const createRestart = (() => {
-      const restart = document.createElement('button')
-      restart.addEventListener('click', () => location.reload())
-      restart.textContent = 'restart'
-      body.appendChild(restart)
-    })()
-
-
-
   })()
 
   const player = (player, name, marker) => {
@@ -51,6 +89,17 @@ const game = (() => {
     name = name //player select name input
     marker = marker // player choose marker
     return { player, name, marker }
+  }
+
+  const playerAssignments = () => {
+    const assignment = document.createElement('div')
+    const player1A = document.createElement('p')
+    player1A.textContent = 'Player 1'
+    player2A.textContent = 'Player 2'
+    assignment.appendChild(player1A)
+    assignment.appendChild(player2A)
+    markers.appendChild(player1A)
+    X.addEventListener('click', playerAssignments)
   }
 
   const gamePlay = (() => {
